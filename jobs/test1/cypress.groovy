7af7f8e3ed46edafed41139e52284aee9464e16a
Chitrang Natu <chit786@gmail.com>
2018-08-04 16:29:38 +0200

    first commit

---
+++ jobs/test1/cypress.groovy
@@ -0,0 +1,79 @@
+
+class JobConfig {
+
+  static final String JENKINS_CREDENTIALS_FOR_GITHUB = "chitsgithub"
+
+  static
+  def basicPipeline(job, repo, includeBranches = "master PR-*", ignoreOnPush = false, buildPR = true, jenkinsfilePath = "Jenkinsfile", interval = 0) {
+
+    job.with {
+      configure {
+        it / factory(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory') {
+          owner(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference: '../..')
+          scriptPath(jenkinsfilePath)
+        }
+
+        it / sources(class: 'jenkins.branch.MultiBranchProject$BranchSourceList') / 'data' / "jenkins.branch.BranchSource" {
+          source(class: "org.jenkinsci.plugins.github_branch_source.GitHubSCMSource") {
+            id(UUID.randomUUID())
+            credentialsId(JENKINS_CREDENTIALS_FOR_GITHUB)
+            repoOwner('xebialabs')
+            repository(repo)
+            ignoreOnPushNotifications(true)
+
+            traits {
+              'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
+                strategyId('1')
+              }
+              if (buildPR) {
+                'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+                  strategyId('1')
+                }
+              }
+              'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
+                includes(includeBranches)
+                excludes()
+              }
+              'org.jenkinsci.plugins.github__branch__source.SSHCheckoutTrait' {
+                credentialsId(JENKINS_CREDENTIALS_FOR_GITHUB)
+              }
+              'jenkins.plugins.git.traits.CloneOptionTrait' {
+                extension(class: "hudson.plugins.git.extensions.impl.CloneOption") {
+                  shallow("false")
+                  noTags("false")
+                  depth("0")
+                  honorRefspec("false")
+                }
+              }
+              if (ignoreOnPush) {
+                'jenkins.plugins.git.traits.IgnoreOnPushNotificationTrait'()
+              }
+            }
+          }
+          strategy(class: "jenkins.branch.DefaultBranchPropertyStrategy") {
+            properties(class: "empty-list")
+          }
+        }
+      }
+
+      orphanedItemStrategy {
+        discardOldItems {
+          daysToKeep(1)
+        }
+      }
+
+      triggers {
+        if (interval > 0) {
+          periodic(interval)
+        }
+      }
+
+    }
+  }
+}
+
+folder('Cypress Development')
+
+def cypressPipeline = multibranchPipelineJob('Cypress Development/cypress Pipeline')
+JobConfig.basicPipeline(cypressPipeline, repo = "jenkins-jobs", includeBranches="master PR-*")
+
